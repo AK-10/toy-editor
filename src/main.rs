ceec7118a0bf50e7a86ab874c8e2a0470b8db054
AK-10 <atsushi524k10@gmail.com>
2022-12-08 00:10:10 +0900

    get file path and read text

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io::{stdin, Read};
+use std::env;
+use std::fs::File;
 
 use toy_editor::terminal::Terminal;
 
@@ -7,11 +9,31 @@ fn control_char(c: char) -> u8 {
 }
 
 fn main() {
+    let path = env::args()
+        .nth(1)
+        .expect("expected file path to first arg, but nothing");
+
+    let file = match File::open(path) {
+        Ok(ref mut f) => {
+            let mut buf = String::with_capacity(4096);
+            if let Ok(_) = f.read_to_string(&mut buf) {
+                buf
+            } else {
+                std::process::exit(1)
+            }
+        }
+        Err(_) => std::process::exit(1)
+    };
+
+    let text: Vec<String> = file
+        .lines()
+        .map(String::from)
+        .collect();
+
     let mut term = Terminal::new();
     term.enable_raw_mode();
 
     while let Some(b) = stdin().bytes().next() {
-        println!("b: {:?}", b);
         // control + qで離脱
         if let Ok(b) = b {
             if b == control_char('q') {
@@ -19,4 +41,6 @@ fn main() {
             }
         }
     }
+
+
 }
